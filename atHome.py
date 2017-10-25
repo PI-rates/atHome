3747ca5a6ed15db3d669f54f1cd8b5f107617cf3
bandang0 <bandang0@yahoo.com>
2017-10-25 21:16:12 +0200

    Fix cpp code in atHome.py.

--- atHome.py
+++ atHome.py
@@ -12,20 +12,21 @@ pierre = 0
 thomas= 0
 # ====  data =============
 cpp_code = """#include <Arduino.h>
-    int ledPin_vincent =  3;
-    int ledPin_pierre = 4;
-    int ledPin_thomas=5;
-    
-    void setup() {
-        pinMode(ledPin_vincent, OUTPUT);
-        pinMode(ledPin_pierre, OUTPUT);
-        pinMode(ledPin_thomas, OUTPUT);
-        digitalWrite(ledPin_vincent, %d);
-        digitalWrite(ledPin_pierre, %d);
-        digitalWrite(ledPin_thomas, %d);
-    };
-    
-    void loop(){};
+
+int ledPin_vincent = 3;
+int ledPin_pierre = 4;
+int ledPin_thomas = 5;
+
+void setup() {
+    pinMode(ledPin_vincent, OUTPUT);
+    pinMode(ledPin_pierre, OUTPUT);
+    pinMode(ledPin_thomas, OUTPUT);
+    digitalWrite(ledPin_vincent, %d);
+    digitalWrite(ledPin_pierre, %d);
+    digitalWrite(ledPin_thomas, %d);
+};
+
+void loop(){};
 """
 
 nmap_cmd = "nmap -sP 192.168.1.1-255 | grep %s"
