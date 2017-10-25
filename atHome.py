d509d4b1f367212c72c2dfc158bc70da2560cb03
bandang0 <bandang0@yahoo.com>
2017-10-25 21:07:11 +0200

    Create new branch and clean down atHome.py.
    The gitignore file contains main.cpp because it is useless to push it to
    the repo every time.

--- atHome.py
+++ atHome.py
@@ -4,23 +4,42 @@
 # Utilisation de la commande NMAP 
 #
 
-
 import os
 import time
 
 vincent = 0
 pierre = 0
 thomas= 0
-# ====  initialisation des diodes a 0 =============
-os.system("echo '#include <Arduino.h>\n int ledPin_vincent =  3;int ledPin_pierre = 4;int ledPin_thomas=5;void setup() {pinMode(ledPin_vincent, OUTPUT);pinMode(ledPin_pierre, OUTPUT);pinMode(ledPin_thomas, OUTPUT);digitalWrite(ledPin_vincent,"+str(vincent)+");digitalWrite(ledPin_pierre,"+str(pierre)+");digitalWrite(ledPin_thomas,"+str(thomas)+");};void loop(){};' > main.cpp")
+# ====  data =============
+cpp_code = """#include <Arduino.h>
+    int ledPin_vincent =  3;
+    int ledPin_pierre = 4;
+    int ledPin_thomas=5;
+    
+    void setup() {
+        pinMode(ledPin_vincent, OUTPUT);
+        pinMode(ledPin_pierre, OUTPUT);
+        pinMode(ledPin_thomas, OUTPUT);
+        digitalWrite(ledPin_vincent, %d);
+        digitalWrite(ledPin_pierre, %d);
+        digitalWrite(ledPin_thomas, %d);
+    };
+    
+    void loop(){};
+"""
 
+nmap_cmd = "nmap -sP 192.168.1.1-255 | grep %s"
+
+# ======== Initialize ==========
+with open("main.cpp", "w") as f:
+    f.write(cpp_code %(0, 0, 0))
 os.system("make upload -s")
 
 while (1):
 
     chgt = 0
     # ========= VINCENT =========
-    nmap_vincent = os.popen("nmap -sP 192.168.1.1-255 | grep 'android-8f93'").readlines()
+    nmap_vincent = os.popen(nmap_cmd %("android-8f93")).readlines()
     
     if nmap_vincent == [] and vincent == 1:
         vincent = 0
@@ -31,7 +50,7 @@ while (1):
         chgt = 1
     
     # ========= PIERRE =========
-    nmap_pierre = os.popen("nmap -sP 192.168.1.1-255 | grep 'Honor_8'").readlines()
+    nmap_pierre = os.popen(nmap_cmd %("Honor_8")).readlines()
 
     if nmap_pierre == [] and pierre == 1:
         pierre = 0
@@ -40,10 +59,9 @@ while (1):
     elif nmap_pierre != [] and pierre == 0:
         pierre = 1
         chgt = 1
-    #==========================
 
     # ========= THOMAS =========
-    nmap_thomas = os.popen("nmap -sP 192.168.1.1-255 | grep 'iPhonedethomas'").readlines()
+    nmap_thomas = os.popen(nmap_cmd %("iPhonedethomas")).readlines()
 
     if nmap_thomas == [] and thomas == 1:
         thomas = 0
@@ -56,6 +74,8 @@ while (1):
 
 
     if chgt == 1: 
-        os.system("echo '#include <Arduino.h>\n int ledPin_vincent =  3;int ledPin_pierre = 4;int ledPin_thomas=5;void setup() {pinMode(ledPin_vincent, OUTPUT);pinMode(ledPin_pierre, OUTPUT);pinMode(ledPin_thomas, OUTPUT);digitalWrite(ledPin_vincent,"+str(vincent)+");digitalWrite(ledPin_pierre,"+str(pierre)+");digitalWrite(ledPin_thomas,"+str(thomas)+");};void loop(){};' > main.cpp")
+        with open("main.cpp", "w") as f:
+            f.write(cpp_code %(vincent, pierre, thomas))
+
         os.system("make upload -s")
     print('OK')
